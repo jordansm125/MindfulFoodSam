fd12150053016c9797ba7a8535b558076fe8106c
jordan schultz mcardle <jordansm125@gmail.com>
2018-07-05 21:39:09 +0800

    Fixes constant refresh

--- src/formHandler.js
+++ src/formHandler.js
@@ -1,38 +1,42 @@
-document.getElementById("form").onload=function(){
+document.getElementById("form").onload=function() {
 //document.addEventListener("DOMContentLoaded", function(){
-	var URL = Object.values(url);
-	console.log(URL[0]);
-	if (countLoadTime=0){
+	//console.log(url)
+	//var URL = Object.values(url);
+	//document.getElementById("form").src = URL[0];
+
+	if (countLoadTime == 0) {
 			console.log(url);
 			//gets array of prefilled url's
 			var prefilledUrl = Object.values(url);
-			console.log(prefilledUrl);
+			//console.log(prefilledUrl);
 			//gets url based on currentLoadTime index
-			document.getElementById("form").src = prefilled[countLoadTime];
-			console.log(prefilled[countLoadTime]);
+			document.getElementById("form").src = prefilledUrl[countLoadTime];
+			console.log(prefilledUrl[0]);
+			//console.log(prefilled[countLoadTime]);
 			//increment countLoadTime
 			countLoadTime++;
+			console.log('HEY HEY HEY HEY HEY HEY HEY HEY ' + countLoadTime);
 			}
-	else if(loadFlag=0){
+	else if (loadFlag == 0) {
 		loadFlag = 1;
 	}
-	else{		
+	else {		
 			//get array of prefilled url's
 			var prefilledUrl = Object.values(url);
 			//get url based on new countLoadTime?
 			document.getElementById("form").src = prefilledUrl[countLoadTime];
 			//resets the loadFlag?
 			loadFlag = 0;
+			console.log('HEY HEY HEY HEY HEY HEY HEY HEY ');
+
 		}
 	};
-var loadFlag = 0;
-var countLoadTime = 0;
-
-
+	
 	//console.log(URL);
 	//document.getElementById("form").src = URL[0];
 
-
+var loadFlag = 0;
+var countLoadTime = 0;
 
 
 //document.getElementById("form").addEventListener("load",function(){
